5030961ae36c53db3190f1de391aea6aae27f0a7
Faris Zhafir Faza <111985990+Marsupilamieue@users.noreply.github.com>
2024-04-16 20:03:59 +0700

    Fix endpoint subscribe

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -5,7 +5,7 @@ use crate::model::notification::Notification;
 use crate::model::subscriber::SubscriberRequest;
 use crate::service::notification::NotificationService;
 
-#[get("/subscriber/<product_type>")]
+#[get("/subscribe/<product_type>")]
 pub fn subscribe(product_type: &str) -> Result<Json<SubscriberRequest>> {
     return match NotificationService::subscribe(product_type) {
         Ok(f) => Ok(Json::from(f)),
